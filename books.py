4fb25165748e14fe895d326de008c50a2d3d05e4
frankafan <frank_fan_1201@outlook.com>
2021-06-09 00:35:30 -0700

    Scrape different months and years.

--- books.py
+++ books.py
@@ -12,17 +12,15 @@ driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 START_YEAR = 2017
 END_YEAR = 2021
 
-
-# for year in range(START_YEAR, END_YEAR + 1):
-#     for month in range(1, 13):
-#         driver.get(f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=daterank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&unfiltered=1&ref=sr_adv_b')
-#
-driver.get('https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=daterank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod=1&field-dateop=During&field-dateyear=2017&unfiltered=1&ref=sr_adv_b')
-
-items = driver.find_elements(By.XPATH, '//div[@class="s-card-container s-overflow-hidden s-include-content-margin s-latency-cf-section s-card-border"]')
-for item in items:
-    book = {}
-    book['title'] = item.find_element(By.XPATH, './/span[@class="a-size-small a-color-base a-text-normal"]').text
-    book['author'] = item.find_elements(By.XPATH, './/span[@class="a-size-mini s-light-weight-text"]')[1].text
-    book['format'] = item.find_element(By.XPATH, './/span[@class="a-size-mini a-color-base s-medium-weight-text a-text-bold"]').text
-    print(book)
+books = []
+for year in range(START_YEAR, END_YEAR + 1):
+    for month in range(1, 13):
+        driver.get(f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=daterank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&unfiltered=1&ref=sr_adv_b')
+        items = driver.find_elements(By.XPATH, '//div[@class="s-card-container s-overflow-hidden s-include-content-margin s-latency-cf-section s-card-border"]')
+        for item in items:
+            book = {}
+            book['title'] = item.find_element(By.XPATH, './/span[@class="a-size-small a-color-base a-text-normal"]').text
+            book['author'] = item.find_elements(By.XPATH, './/span[@class="a-size-mini s-light-weight-text"]')[1].text
+            book['format'] = item.find_element(By.XPATH, './/span[@class="a-size-mini a-color-base s-medium-weight-text a-text-bold"]').text
+            books.append(book)
+print(books)
