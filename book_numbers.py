526d3ce6c523bd643f99031f1d022e4a30a9a813
frankafan <frank_fan_1201@outlook.com>
2021-06-14 11:53:54 -0700

    Obtain number of results.

--- book_numbers.py
+++ book_numbers.py
@@ -8,3 +8,23 @@ mobile_emulation = {
 chrome_options = Options()
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
+
+
+driver.get('https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=daterank&Adv-Srch-Books-Submit.x=19&Adv-Srch-Books-Submit.y=18&field-datemod=3&field-dateop=During&field-dateyear=2021&unfiltered=1&ref=sr_adv_b')
+
+data = []
+
+for element1 in driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]'):
+    print(1)
+    if element1.text == 'Filters':
+        driver.implicitly_wait(1)
+        element1.click()
+        for element2 in driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]'):
+            if element2.text.split(' ')[0] == 'Show':
+                data.append({
+                    'results1': element2.text.split(' ')[1],
+                })
+                print(driver.page_source)
+                break
+        break
+print(data)
