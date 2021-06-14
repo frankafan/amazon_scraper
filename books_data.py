6adcfbc9e08aecb0841959be7f57e5c777187609
frankafan <frank_fan_1201@outlook.com>
2021-06-13 17:35:24 -0700

    Combine book data from each month.

---
+++ books_data.py
@@ -0,0 +1,17 @@
+import json
+
+START_YEAR = 2017
+END_YEAR = 2021
+
+books = []
+for year in range(START_YEAR, END_YEAR + 1):
+    for month in range(1, 13):
+        try:
+            file = open(f'{year}-{month}.txt', 'r')
+            books.extend(json.loads(file.read()))
+        except:
+            print(f'Missing {year}-{month}.txt')
+            continue
+
+with open('data.txt', 'w') as outfile:
+    json.dump(books, outfile)
