6a3253324286236370573af06f5970a22e30ee2e
frankafan <frank_fan_1201@outlook.com>
2021-06-14 12:58:04 -0700

    Save data.

--- book_numbers.py
+++ book_numbers.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import json
 
 mobile_emulation = {
-   "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
-   "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
+    "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
 chrome_options = Options()
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
@@ -12,6 +13,7 @@ driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 
 def find_filter_items(driver=driver):
     return driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
+
 
 data = []
 
@@ -42,7 +44,9 @@ for year in range(START_YEAR, END_YEAR + 1):
                     for i in range(len(elements)):
                         if elements[i].text == 'English':
                             # driver.execute_script("arguments[0].scrollIntoView();", elements[i])
-                            driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements(By.XPATH, './/span[@class="a-size-medium a-color-base"]')[-5])
+                            driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements(By.XPATH,
+                                                                                                         './/span[@class="a-size-medium a-color-base"]')[
+                                -5])
                             elements[i].click()
                             elements = find_filter_items(driver)
                             break
@@ -61,3 +65,5 @@ for year in range(START_YEAR, END_YEAR + 1):
                 print(month_data)
                 break
 print(data)
+with open(f'num_results.txt', 'w') as outfile:
+    json.dump(data, outfile)
