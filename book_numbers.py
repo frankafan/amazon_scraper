8f9758f752a0e4dcc447bae1c63117a0a187e532
frankafan <frank_fan_1201@outlook.com>
2021-06-14 12:50:35 -0700

    Loop through each month + create function.

--- book_numbers.py
+++ book_numbers.py
@@ -9,34 +9,46 @@ chrome_options = Options()
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 
+
+def find_filter_items(driver=driver):
+    return driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
+
 data = []
 
-month_data = {}
-while not month_data:
-    driver.get(
-        'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=daterank&Adv-Srch-Books-Submit.x=19&Adv-Srch-Books-Submit.y=18&field-datemod=3&field-dateop=During&field-dateyear=2021&unfiltered=1&ref=sr_adv_b')
-    elements = driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
-    for i in range(len(elements)):
-        if elements[i].text == 'Filters':
-            elements[i].click()
-            elements = driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
-            break
-    for i in range(len(elements)):
-        if elements[i].text.split(' ')[0] == 'Show':
-            month_data['result1'] = elements[i].text.split(' ')[1]
-            elements = driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
-            break
-    for i in range(len(elements)):
-        if elements[i].text == 'English':
-            # driver.execute_script("arguments[0].scrollIntoView();", elements[i])
-            driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements(By.XPATH, './/span[@class="a-size-medium a-color-base"]')[-5])
-            elements[i].click()
-            elements = driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
-            break
-    for i in range(len(elements)):
-        if elements[i].text.split(' ')[0] == 'Show':
-            month_data['result2'] = elements[i].text.split(' ')[1]
-            elements = driver.find_elements(By.XPATH, './/span[@class="a-size-small a-color-base"]')
-            break
-data.append(month_data)
+START_YEAR = 2017
+END_YEAR = 2021
+for year in range(START_YEAR, END_YEAR + 1):
+    for month in range(1, 13):
+        print(f'Year: {year}')
+        print(f'Month: {month}')
+
+        month_data = {}
+        while not month_data:
+            driver.get(
+                f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
+            elements = find_filter_items(driver)
+            for i in range(len(elements)):
+                if elements[i].text == 'Filters':
+                    elements[i].click()
+                    elements = find_filter_items(driver)
+                    break
+            for i in range(len(elements)):
+                if elements[i].text.split(' ')[0] == 'Show':
+                    month_data['result1'] = elements[i].text.split(' ')[1]
+                    elements = find_filter_items(driver)
+                    break
+            for i in range(len(elements)):
+                if elements[i].text == 'English':
+                    # driver.execute_script("arguments[0].scrollIntoView();", elements[i])
+                    driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements(By.XPATH, './/span[@class="a-size-medium a-color-base"]')[-5])
+                    elements[i].click()
+                    elements = find_filter_items(driver)
+                    break
+            for i in range(len(elements)):
+                if elements[i].text.split(' ')[0] == 'Show':
+                    month_data['result2'] = elements[i].text.split(' ')[1]
+                    elements = find_filter_items(driver)
+                    break
+        data.append(month_data)
+        print(month_data)
 print(data)
