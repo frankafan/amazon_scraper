8388c0e4989a1ab8b65a8d01fd892ed8e22462c0
Frank <frank_fan_1201@outlook.com>
2021-06-28 15:46:32 -0700

    Loop through subcategories and retrieve number of results.

--- num_books_results_subgroups.py
+++ num_books_results_subgroups.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 import json
 
 mobile_emulation = {
-    "deviceMetrics": {"width": 300, "height": 600, "pixelRatio": 3.0},
+    "deviceMetrics": {"width": 640, "height": 1080, "pixelRatio": 1.0},
     "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
 chrome_options = Options()
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
@@ -62,33 +62,43 @@ for year in range(START_YEAR, END_YEAR + 1):
         while not (int(CURRENT_DATE.split('-')[0]) < year and int(CURRENT_DATE.split('-')[1]) < month):
             try:
                 month_data = {}
-                # while not (('result1' in month_data.keys() and 'result1' in month_data.keys()) and (month_data['result1'] != None and month_data['result2'] != None)):
-                #     driver.get(
-                #         f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
-                #     elements = find_filter_items(driver)
-                #     click_filter_button(elements)
-                #     elements = find_filter_items(driver)
-                #     month_data['result1'] = find_num_results(elements)
-                #     for i in range(len(elements)):
-                #         if elements[i].text == 'English':
-                #             driver.execute_script("arguments[0].scrollIntoView();", elements[i])
-                #             elements[i].click()
-                #             driver.refresh()
-                #             elements = find_filter_items(driver)
-                #             break
-                #     click_filter_button(elements)
-                #     month_data['result2'] = find_num_results(elements)
+                while not ('All' in month_data.keys() and month_data['All'] != None):
+                    driver.get(
+                        f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
+                    elements = find_filter_items(driver)
+                    click_filter_button(elements)
+                    elements = find_filter_items(driver)
+                    month_data['All'] = find_num_results(elements)
+                    print(f'All: {month_data["All"]}')
 
-                driver.get(
-                    f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
                 available_subcategories = []
-                elements = find_filter_items(driver)
-                click_filter_button(elements)
-                elements = find_filter_items(driver)
-                for i in range(len(elements)):
-                    if elements[i].text in subcategories:
-                        available_subcategories.append(elements[i].text)
-                print(available_subcategories)
+                while len(available_subcategories) == 0:
+                    driver.get(
+                        f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
+                    elements = find_filter_items(driver)
+                    click_filter_button(elements)
+                    elements = find_filter_items(driver)
+                    for i in range(len(elements)):
+                        if elements[i].text in subcategories:
+                            available_subcategories.append(elements[i].text)
+                for subcategory in available_subcategories:
+                    while not (subcategory in month_data.keys()):
+                        driver.get(
+                            f'https://www.amazon.com/s?i=stripbooks&rh=n%3A5%2Cp_n_condition-type%3A1294423011%2Cp_20%3AEnglish&s=date-desc-rank&Adv-Srch-Books-Submit.x=24&Adv-Srch-Books-Submit.y=12&field-datemod={month}&field-dateop=During&field-dateyear={year}&qid=1623224173&unfiltered=1&ref=aa_sbox_sort')
+                        elements = find_filter_items(driver)
+                        click_filter_button(elements)
+                        elements = find_filter_items(driver)
+                        for i in range(len(elements)):
+                            if elements[i].text == subcategory:
+                                driver.execute_script("arguments[0].scrollIntoView(true);", elements[0])
+                                elements[i].click()
+                                driver.refresh()
+                                elements = find_filter_items(driver)
+                                break
+                        click_filter_button(elements)
+                        month_data[subcategory] = find_num_results(elements)
+                        available_subcategories.pop(available_subcategories.index(subcategory))
+                        print(f'{subcategory}: {month_data[subcategory]}')
                 month_data['year'] = year
                 month_data['month'] = month
                 data.append(month_data)
@@ -98,6 +108,6 @@ for year in range(START_YEAR, END_YEAR + 1):
             else:
                 print(month_data)
                 break
-print(data)
-with open(f'num_results.txt', 'w') as outfile:
-    json.dump(data, outfile)
+# print(data)
+# with open(f'num_results.txt', 'w') as outfile:
+#     json.dump(data, outfile)
